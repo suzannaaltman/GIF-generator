7853a5f02cc167446eb4bcc079cfe08346e8a14d
suzannaaltman <suzannaaltman@gmail.com>
2016-04-07 13:06:58 -0500

    switch from jQuery to Angular, update styling

--- app.js
+++ app.js
@@ -1,23 +1,18 @@
-$(function(){
+var app = angular.module('gifGenerator', []);
 
-  $("#getGIF").on("click", function(){
-    $.get("http://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC").done(function(response){
-      addGif(response);
+app.controller('showGif', function($scope, $http){
+  $scope.getGif = function(){
+    $http.get("http://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC").then(function(response){
+      $scope.gif = response.data.data.image_url;
+      console.log(response);
     })
-  })
+  }
 
-  $("#tagSearch").on("submit", function(event){
-    event.preventDefault();
-    var getObject = $("#tagSearch").serializeArray();
-    var keywordTag = getObject[0].value;
-    $.get("http://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&tag=" + keywordTag).done(function(response){
-      addGif(response);
+  $scope.searchGif = function(){
+    $http.get("http://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&tag=" + $scope.keyword).then(function(response){
+      $scope.gif = response.data.data.image_url;
+      $scope.keyword = '';
+      console.log(response);
     })
-  })
-
-function addGif(response){
-  $("#imageHolder").empty();
-  $("#imageHolder").append("<p><img src=\"" + response.data.image_url + "\" /></div>");
-}
-
-});
+  }
+})
